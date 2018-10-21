aa38222c805c5b7cc25f26c91e49e0b0b5a8f6b5
John Akerman <john.c.akerman@gmail.com>
2018-10-21 22:36:36 +0100

    Added test stub for throwing errors, still needs work

--- test/data.spec.js
+++ test/data.spec.js
@@ -11,19 +11,12 @@ describe('#parseData()', () => {
   it('should error when undefined data is provided', async () => {
     assert(await Data.parseData(undefined), 'No album data provided')
   })
-  //
+
   // it('should error when numbers are provided', async () => {
-  //   await expect(() => Data.parseData(123)).to.throw(Error, 'Issue with screen object')
-  //   // assert(await Data.parseData(123), 'No URL provided')
-  //   // await expect(async () => Data.parseData(123)).to.throw(Error, 'Issue with screen object')
-  // })
-  //
-  // it('should error when object are provided', async () => {
-  //   expect(() => Data.parseData('{"test": "test2"}')).to.throw(Error, 'Issue with screen object')
-  //   // await expect(async () => Data.parseData(123)).to.throw(Error, 'Issue with screen object')
+  //   await expect(async () => Data.parseData(123)).to.throw(Error, 'Issue with screen object')
   // })
 })
 
 // function t (x) {
-//   throw new Error('Test Error')
+//   throw new Error('Issue with screen object')
 // }
